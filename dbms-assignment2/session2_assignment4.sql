f2d0d0530a4090b38f52b4cca2495dbd0f61e615
meta-pulkit-gupta <pulkit.gupta@metacube.com>
2016-08-12 10:33:51 +0530

    Add files via upload

--- dbms-assignment2/session2_assignment4.sql
+++ dbms-assignment2/session2_assignment4.sql
@@ -1,7 +1,12 @@
+/* Assignment 4 */
+
+/* Create database */
 CREATE DATABASE PinDirectory;
 
+/* Selecting Databse */
 USE pindirectory;
 
+/* Creating Tables */
 CREATE TABLE city(
 	pincode INT(6) NOT NULL,
 	city_name VARCHAR(20) NOT NULL,
@@ -9,6 +14,7 @@ CREATE TABLE city(
 	PRIMARY KEY(pincode, state_name)
 );
 
+/* Inserting data into tables */
 INSERT INTO city VALUE (302020, 'Jaipur', 'Rajasthan');
 INSERT INTO city VALUE (321001, 'Bharatpur', 'Rajasthan');
 INSERT INTO city VALUE (408702, 'Jammu', 'Jammu and Kashmir');
@@ -18,6 +24,8 @@ INSERT INTO city VALUE (302020, 'Jamnagar', 'Maharashtra');
 INSERT INTO city VALUE (400001, 'Mumbai', 'Maharashtra');
 INSERT INTO city VALUE (400020, 'Nagpur', 'Maharashtra');
 
+/* To display zipcode, city name and state name
+ * ordered by state name and then by city name */
 SELECT pincode AS 'Pincode', city_name AS 'City Name', state_name AS 'State Name'
 	FROM city
 	ORDER BY  state_name, city_name;
